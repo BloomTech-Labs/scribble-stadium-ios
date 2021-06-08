422f795fe82c281f5a4f00d3f43f906d926e5001
Kenneth Jones <kennethjones1991@outlook.com>
2021-06-07 20:52:15 -0600

    Updated CoreDataStack and ParentRepresentation and Parent+Convenience to work with new StorySquad model

--- StorySquad/Model/ParentRepresentation.swift
+++ StorySquad/Model/ParentRepresentation.swift
@@ -9,43 +9,24 @@
 import Foundation
 
 struct ParentRepresentation: Codable {
+    let parentID: Int
+    let name: String
+    let email: String
+    let pin: String
     
     private enum ParentCodingKeys: String, CodingKey {
-        case me
-        
-        enum MeParentCodingKeys: String, CodingKey {
-            
-            case email
-            case children
-            case id
-        }
+        case id = "ID"
+        case name = "Name"
+        case email = "Email"
+        case pin = "PIN"
     }
-    
-    var email: String
-    var children: [ChildInParentRepresentation]?
-    var id: Int16
-    
-    var password: String?
-    var name: String?
-    var pin: Int16?
     
     init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: ParentCodingKeys.self)
-        let meContainer = try container.nestedContainer(keyedBy: ParentCodingKeys.MeParentCodingKeys.self, forKey: .me)
         
-        email = try meContainer.decode(String.self, forKey: .email)
-        children = try meContainer.decode([ChildInParentRepresentation].self, forKey: .children)
-        id = try meContainer.decode(Int16.self, forKey: .id)
-        
-        //        name = try meContainer.decode(String.self, forKey: .name)
-        //        password = try meContainer.decode(String?.self, forKey: .password)
-        //        pin = try meContainer.decode(Int16.self, forKey: .pin)
-    }
-    
-    init(id: Int16, email: String, children: [ChildInParentRepresentation] = []) {
-
-        self.id = id
-        self.email = email
-        self.children = children
+        parentID = try container.decode(Int.self, forKey: .id)
+        name = try container.decode(String.self, forKey: .name)
+        email = try container.decode(String.self, forKey: .email)
+        pin = try container.decode(String.self, forKey: .pin)
     }
 }
