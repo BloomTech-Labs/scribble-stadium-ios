1431676e2184eee6bc1846646420f11149330dae
Waseem Idelbi <idelbiw@gmail.com>
2021-09-21 23:38:04 -0400

    just created a file for a UICollectionViewCell with an imageView that will serve to display an image

---
+++ StorySquad/View Cell/ScribbleCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ScribbleCollectionViewCell.swift
+//  StorySquad
+//
+//  Created by Waseem Idelbi on 9/21/21.
+//  Copyright © 2021 Spencer Curtis. All rights reserved.
+//
+
+import UIKit
+
+class ScribbleCollectionViewCell: UICollectionViewCell {
+    var imageView: UIImageView!
+}
